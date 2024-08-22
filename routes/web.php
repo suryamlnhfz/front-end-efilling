2b9e83f6ee68b3ba6177dba02f5fcd6abad8544e
Arafie2603 <arrafiesetiawan@gmail.com>
2024-08-22 12:07:01 +0700

    terbaru

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/', function () {
-    return view('dashboard.index');
+    return view('components.dashboard.index');
 });
